e028fd2bb013bc3c52037721ac674cfb5afb108d
Henry <henry@pragmastrategy.com>
2020-06-15 21:15:08 +0700

    Update project

--- app/src/main/java/com/ldt/musicr/api/providers/EntertainmentProvider.java
+++ app/src/main/java/com/ldt/musicr/api/providers/EntertainmentProvider.java
@@ -1,5 +1,6 @@
 package com.ldt.musicr.api.providers;
 
+import android.media.MediaMetadataRetriever;
 import android.util.Log;
 import com.ldt.musicr.api.customModels.ApiResponse;
 import com.ldt.musicr.api.models.Song;
@@ -69,5 +70,19 @@ public class EntertainmentProvider {
             Log.e(TAG, e.getMessage());
             return new ArrayList<>();
         }
+
+    }
+
+    public long getSongDuration(String source)
+    {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(source);
+        String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        long timeInmillisec = Long.parseLong( time );
+        long duration = timeInmillisec / 1000;
+        long hours = duration / 3600;
+        long minutes = (duration - hours * 3600) / 60;
+        long seconds = duration - (hours * 3600 + minutes * 60);
+        return duration;
     }
 }
